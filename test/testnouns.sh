9c99beaec7466133fc3ba361c3d1c7768b04504e
Lene Antonsen <lene.antonsen@uit.no>
2014-04-30 23:40:06 +0000

    Improvements
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/crk@93412 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/testnouns.sh
+++ test/testnouns.sh
@@ -1,9 +1,9 @@
-grep ";" $GTHOME/langs/crk/src/morphology/stems/nouns.lexc |egrep -v '^(<|\!|@|AN|IN|IR)' | tr ':+' ' ' | cut -d ' ' -f1 | sort -u | grep -v "^$" > $GTHOME/langs/crk/lemmas.txt
-grep ";" $GTHOME/langs/crk/src/morphology/stems/nouns.lexc |egrep -v '^(<|\!|@|AN|IN|IR)' | grep ' IN' |tr ':+' ' ' | cut -d ' ' -f1 | grep -v "^$" |sed 's/$/+N+IN+Sg/' |$HLOOKUP $GTHOME/langs/crk/src/generator-gt-norm.hfst | cut -f2 | grep -v "^$" |sort -u > $GTHOME/langs/crk/anlemmas.txt
-grep ";" $GTHOME/langs/crk/src/morphology/stems/nouns.lexc |egrep -v '^(<|\!|@|AN|IN|IR)' | egrep ' (AN|IRREGULARDECL)' |tr ':+' ' ' | cut -d ' ' -f1 | grep -v "^$" |sed 's/$/+N+AN+Sg/' |$HLOOKUP $GTHOME/langs/crk/src/generator-gt-norm.hfst | cut -f2 | grep -v "^$" |sort -u >> $GTHOME/langs/crk/anlemmas.txt
-grep ";" $GTHOME/langs/crk/src/morphology/stems/nouns.lexc |egrep -v '^(<|\!|@|AN|IN|IR)' | egrep 'N\+IN\+Sg\+Px3Sg' |tr ':+' ' ' | cut -d ' ' -f1 | grep -v "^$" |sed 's/$/+N+IN+Sg+Px3Sg/' |$HLOOKUP $GTHOME/langs/crk/src/generator-gt-norm.hfst | cut -f2 | grep -v "^$" |sort -u >> $GTHOME/langs/crk/anlemmas.txt
-sort -u -o $GTHOME/langs/crk/anlemmas.txt $GTHOME/langs/crk/anlemmas.txt
-comm -23 $GTHOME/langs/crk/lemmas.txt $GTHOME/langs/crk/anlemmas.txt > $GTHOME/langs/crk/failedlemmas.txt
-see $GTHOME/langs/crk/failedlemmas.txt
+grep ";" $GTHOME/langs/crk/src/morphology/stems/nouns.lexc |egrep -v '^(<|\!|@|AN|IN|IR)' | tr ':+' ' ' | cut -d ' ' -f1 | sort -u | grep -v "^$" > $GTHOME/langs/crk/test/lemmas.txt
+grep ";" $GTHOME/langs/crk/src/morphology/stems/nouns.lexc |egrep -v '^(<|\!|@|AN|IN|IR)' | grep ' IN' |tr ':+' ' ' | cut -d ' ' -f1 | grep -v "^$" |sed 's/$/+N+IN+Sg/' |$HLOOKUP $GTHOME/langs/crk/src/generator-gt-norm.hfst | cut -f2 | grep -v "^$" |sort -u > $GTHOME/langs/crk/test/anlemmas.txt
+grep ";" $GTHOME/langs/crk/src/morphology/stems/nouns.lexc |egrep -v '^(<|\!|@|AN|IN|IR)' | egrep ' (AN|IRREGULARDECL)' |tr ':+' ' ' | cut -d ' ' -f1 | grep -v "^$" |sed 's/$/+N+AN+Sg/' |$HLOOKUP $GTHOME/langs/crk/src/generator-gt-norm.hfst | cut -f2 | grep -v "^$" |sort -u >> $GTHOME/langs/crk/test/anlemmas.txt
+grep ";" $GTHOME/langs/crk/src/morphology/stems/nouns.lexc |egrep -v '^(<|\!|@|AN|IN|IR)' | egrep 'N\+IN\+Sg\+Px3Sg' |tr ':+' ' ' | cut -d ' ' -f1 | grep -v "^$" |sed 's/$/+N+IN+Sg+Px3Sg/' |$HLOOKUP $GTHOME/langs/crk/src/generator-gt-norm.hfst | cut -f2 | grep -v "^$" |sort -u >> $GTHOME/langs/crk/test/anlemmas.txt
+sort -u -o $GTHOME/langs/crk/test/anlemmas.txt $GTHOME/langs/crk/test/anlemmas.txt
+comm -23 $GTHOME/langs/crk/test/lemmas.txt $GTHOME/langs/crk/test/anlemmas.txt > $GTHOME/langs/crk/test/notgeneratedlemmas.txt
+see $GTHOME/langs/crk/test/notgeneratedlemmas.txt
 
 
