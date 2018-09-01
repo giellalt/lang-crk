9ae81c5a07a1c2d2a31a6476fb04cd2db7cfe2f5
Antti Arppe <arppe@ualberta.ca>
2018-09-01 00:02:10 +0000

    Generalized shell-script to apply to both single *.layout files and all appropriately named *.layout files associated with the *.paradigm file.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/crk@170251 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- inc/paradigms/verify-paradigm-layout.sh
+++ inc/paradigms/verify-paradigm-layout.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# Usage:
+#    A single specified layout associated with paradigm
+#    ./verify-paradigm-layout.sh verb-ta.paradigm verb-ta-full.layout
+#
+#    OR
+#
+#    All layouts associated with paradigm:
+#    ./verify-paradigm-layout.sh verb-ta.paradigm
+#
+# Author: Antti Arppe, ALTLab (2018)
+
 gawk -v PARADIGM_FILE=$1 -v LAYOUT_FILE=$2 'BEGIN { paradigm_file=PARADIGM_FILE; layout_file=LAYOUT_FILE;
 
-# One could conceive of automating this for all *.layout files wrt to the corresponding *.paradigm file
 # paradigm_file=layout_file; sub("[-]((basic)|(extended)|(full)).layout",".paradigm", paradigm_file);
 
 while((getline < paradigm_file)!=0)
@@ -12,33 +22,45 @@ while((getline < paradigm_file)!=0)
      if(index(line,"{{ lemma }}")!=0)
        paradigm[p_nr]=line;
    }
-# print "BEGIN"
-# for(p in paradigm)
-#    print p, paradigm[p];
-# print "END"
-print "PARADIGM FILE: "paradigm_file" <- LAYOUT FILE: "layout_file;
-while((getline < layout_file)!=0)
-   { if(index($0,"|")!=0)
-     { nf = split($0,f,"[ \t]*\\|[ \t]*");
-       for(i=1; i<=nf; i++)
-          { sub("^[:space:]*","",f[i]); 
-            sub("[:space:]*$","",f[i]);
-            # print i, length(f[i]), f[i];
-            n_matches=0; matches="";
-            if(index(f[i],"\"")==0 && length(f[i])>0)
-              { f_regex=f[i];
-                sub("\\*",".*",f_regex);
-                gsub("\\+","\\+",f_regex);
-                # print f_regex;
-                for(j=1; j<=p_nr; j++)
-                  if(match(paradigm[j],f_regex)!=0)
-                    { n_matches++;
-                      matches=matches j":"paradigm[j]" ";
-                    }
-                if(n_matches!=1)
-                  print f[i]" - "n_matches" match(es): "matches;
-              }
-          }
+
+if(layout_file=="")
+  { file_prefix=paradigm_file; sub(".paradigm$","",file_prefix);
+    while(("ls "file_prefix"*.layout" | getline)!=0)
+         layout_files[$0]=$0;
+  }
+else
+  layout_files[layout_file]=layout_file;
+
+for(l in layout_files)
+{
+  print "PARADIGM FILE: "paradigm_file" <- LAYOUT FILE: "layout_files[l];
+  print "-----";
+  while(l!="" && (getline < layout_files[l])!=0)
+     { if(index($0,"|")!=0)
+       { nf = split($0,f,"[ \t]*\\|[ \t]*");
+         for(i=1; i<=nf; i++)
+            { sub("^[:space:]*","",f[i]); 
+              sub("[:space:]*$","",f[i]);
+              # print i, length(f[i]), f[i];
+              n_matches=0; matches="";
+              if(index(f[i],"\"")==0 && length(f[i])>0)
+                { f_regex=f[i];
+                  sub("\\*",".*",f_regex);
+                  gsub("\\+","\\+",f_regex);
+                  # print f_regex;
+                  for(j=1; j<=p_nr; j++)
+                    if(match(paradigm[j],f_regex)!=0)
+                      { n_matches++;
+                        matches=matches j":"paradigm[j]" ";
+                      }
+                  if(n_matches==0)
+                    matches="???";
+                  if(n_matches!=1)
+                    print f[i]" - "n_matches" match(es): "matches;
+                }
+            }
+       }
      }
-   }
-}'
+  print "-----";
+  print "";
+} }'
