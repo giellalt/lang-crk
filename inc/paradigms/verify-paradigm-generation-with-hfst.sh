94713b3ed6a1314292c0d1fbe421263523e68244
Antti Arppe <arppe@ualberta.ca>
2018-09-06 06:40:32 +0000

    Improved counting of word form index to apply to potentially multi-form cohorts than line number; counting still starts from first form. Also expanded final reporting to cover both entirely successful cases, as wel as those when forms cannot be generated by the (H)FST for whatever reason.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/crk@170414 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- inc/paradigms/verify-paradigm-generation-with-hfst.sh
+++ inc/paradigms/verify-paradigm-generation-with-hfst.sh
@@ -5,15 +5,19 @@
 
 cat $1 |
 gawk -v LEMMA=$2 'BEGIN { lemma=LEMMA; }
-$0 ~ /\{\{ lemma \}\}/ { sub("\\{\\{ lemma \\}\\}",lemma,$0); print; }' |
+$0 ~ /\{\{ lemma \}\}/ { sub("\\{\\{ lemma \\}\\}",lemma,$0); print; }' | # less; exit 0;
 
 hfst-lookup -q $3 |
 
-gawk -v PARADIGM_FILE=$1 -v FST_FILE=$3 'BEGIN { print "PARADIGM FILE: "PARADIGM_FILE" - HFST: "FST_FILE; }
+gawk -v PARADIGM_FILE=$1 -v FST_FILE=$3 'BEGIN { FS="\n"; RS=""; err=0;
+  print "PARADIGM FILE: "PARADIGM_FILE" - HFST: "FST_FILE; }
 index($0,"+?")!=0 { err++; print "Illformed - form #"NR": ",$0; }
-END { print "Unable to generate: "err" out of "NR" form(s).";
-      if(err==0)
-        exit 0;
+END { if(err==0)
+        { print "Able to generate: all "NR" form(s).";
+          exit 0;
+        }
       else
-        exit 1
+        { print "Unable to generate: "err" out of "NR" form(s).";
+          exit 1;
+        }
 }'
