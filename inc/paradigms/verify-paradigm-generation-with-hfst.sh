af775e49d84a34891e13fa83f74ccaa9f2c6311a
Eddie Santos <easantos@ualberta.ca>
2018-09-06 21:02:52 +0000

    Verify ALL of the paradigm files with one command!
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/crk@170474 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- inc/paradigms/verify-paradigm-generation-with-hfst.sh
+++ inc/paradigms/verify-paradigm-generation-with-hfst.sh
@@ -3,11 +3,11 @@
 # Usage:
 #     verify-paradigm-generation-with-hfst.sh verb-ta.paradigm wâpamêw PATH-to-NORMATIVE-GENERATOR-HFST
 
-cat $1 |
-gawk -v LEMMA=$2 'BEGIN { lemma=LEMMA; }
+<"$1" \
+gawk -v LEMMA="$2" 'BEGIN { lemma=LEMMA; }
 $0 ~ /\{\{ lemma \}\}/ { sub("\\{\\{ lemma \\}\\}",lemma,$0); print; }' | # less; exit 0;
 
-hfst-lookup -q $3 |
+hfst-lookup -q "$3" |
 
 gawk -v PARADIGM_FILE=$1 -v FST_FILE=$3 'BEGIN { FS="\n"; RS=""; err=0;
   print "PARADIGM FILE: "PARADIGM_FILE" - HFST: "FST_FILE; }
