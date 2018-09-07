837a3628062a50dff2c8b4604bf1064786004185
Antti Arppe <arppe@ualberta.ca>
2018-09-07 08:48:42 +0000

    Added debug output after HFST which is commented out except when debugging.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/crk@170514 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- inc/paradigms/verify-paradigm-generation-with-hfst.sh
+++ inc/paradigms/verify-paradigm-generation-with-hfst.sh
@@ -14,7 +14,7 @@
 gawk -v LEMMA="$2" 'BEGIN { lemma=LEMMA; }
 $0 ~ /\{\{ lemma \}\}/ { sub("\\{\\{ lemma \\}\\}",lemma,$0); print; }' | # less; exit 0;
 
-hfst-lookup -q "$3" |
+hfst-lookup -q "$3" | # less; exit 0;
 
 gawk -v PARADIGM_FILE=$1 -v FST_FILE=$3 'BEGIN { FS="\n"; RS=""; err=0;
   print "PARADIGM FILE: "PARADIGM_FILE" - HFST: "FST_FILE; }
