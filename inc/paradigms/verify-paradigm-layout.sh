4fb66fa999de2665f9702b3814b7e1530c661c5e
Antti Arppe <arppe@ualberta.ca>
2018-09-09 23:19:28 +0000

    Fixed automated identification of *.layout files; improved final summary reporting on identified discrepancies.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/crk@170617 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- inc/paradigms/verify-paradigm-layout.sh
+++ inc/paradigms/verify-paradigm-layout.sh
@@ -20,12 +20,15 @@ while((getline < paradigm_file)!=0)
      sub("^[ \t]*",""); sub("[ \t]*$","");
      line=$0;
      if(index(line,"{{ lemma }}")!=0)
-       paradigm[p_nr]=line;
+       { paradigm[p_nr]=line;
+         sub("^.*{{ lemma }}\\+","",line);
+         suffix[p_nr]=line;
+       }
    }
 
 if(layout_file=="")
-  { file_prefix=paradigm_file; sub(".paradigm$","",file_prefix);
-    while(("ls "file_prefix"*.layout" | getline)!=0)
+  { file_prefix=paradigm_file; sub("\\.paradigm$","",file_prefix);
+    while(("ls "file_prefix"-*.layout" | getline)!=0)
          layout_files[$0]=$0;
   }
 else
@@ -34,32 +37,57 @@ else
 for(l in layout_files)
 {
   print "LAYOUT FILE: "layout_files[l]" -> PARADIGM FILE: "paradigm_file;
+  body=0; n_cells=0; z_err=0; d_err=0;
   while(l!="" && (getline < layout_files[l])!=0)
-     { if(index($0,"|")!=0)
-       { nf = split($0,f,"[ \t]*[\\|][ \t]*");
+     { if(body==1)
+       { if(index($0,"|")==0)
+           nf = split($0,f,"\t");
+         else
+           nf = split($0,f,"[ \t]*[\\|][ \t]*");
          for(i=1; i<=nf; i++)
             { sub("^[ \t]*","",f[i]); 
               sub("[ \t]*$","",f[i]);
               # print i, length(f[i]), f[i];
               n_matches=0; matches="";
               if(index(f[i],"\"")==0 && length(f[i])>0)
-                { f_regex=f[i];
+                { f_regex=f[i]; n_cells++;
                   sub("\\*",".*",f_regex);
                   gsub("\\+","\\+",f_regex);
                   # print f_regex;
                   for(j=1; j<=p_nr; j++)
-                    if(match(paradigm[j],f_regex)!=0)
-                      { n_matches++;
-                        matches=matches j":"paradigm[j]" ";
+                    if(match(f_regex,"(^[=\\^])|([\\$]$)")!=0)
+                      { f_suffix=f_regex; # print f_suffix;
+                        if(match(f_suffix,"^=")!=0)
+                          { sub("^=","",f_suffix);
+                            f_suffix="^"f_suffix"$";
+                          }
+                        if(match(suffix[j],f_suffix)!=0)
+                          { n_matches++;
+                            matches=matches j":"paradigm[j]" ";
+                          }
                       }
+                    else
+                      if(match(paradigm[j],f_regex)!=0)
+                        { n_matches++;
+                          matches=matches j":"paradigm[j]" ";
+                        }
                   if(n_matches==0)
-                    { matches="???"; err++; }
+                    { matches="???"; z_err++; }
+                  if(n_matches>=2)
+                      d_err++;
                   if(n_matches!=1)
                     { printf "%s - %i match(es): %s\n", f[i], n_matches, matches; err++; }
                 }
             }
        }
-     }
+    if(match($0,"^[-][-]")!=0)
+      body=1;
+  }
+
+  if(d_err!=0 || z_err!=0)
+    printf "SUMMARY: %i duplicate / %i zero match(es) out of %i cells.\n", d_err, z_err, n_cells;
+  else
+    printf "SUMMARY: %i unique match(es) out of %i cell(s).\n", n_cells, n_cells;
   print "-----";
 }
   if(err!=0)
