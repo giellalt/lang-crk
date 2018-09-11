463ee8540e29df83f58556b75c2addbcf1b5b4a2
antti.arppe@iki.fi <antti.arppe@iki.fi>
2018-09-11 08:19:28 +0000

    Revised presentation of inconsistencies to indicate problematic lines per tooltip specification or tooltip reference (without matches in tooltip specifications), or odd number of quotes. Thus, now outputs a more concise representation easing judgement of cells not needing a tooltip expansion.

--- inc/paradigms/verify-layout-tooltips.sh
+++ inc/paradigms/verify-layout-tooltips.sh
@@ -22,9 +22,15 @@ gawk -v LAYOUT_FILE=$1 'BEGIN { layout_file=LAYOUT_FILE;
       sub("^[ \t]+","",f[1]);
       sub(":$","",f[1]);
       if(f[1] in tt)
-        print "Duplicate tooltip - line: #"NR": "$0; # > "/dev/stderr";
+        { # print "Duplicate tooltip - line: #"NR": "$0; # > "/dev/stderr";
+          tt[f[1]]=tt[f[1]]" "NR;
+          d_err++; err++;
+          n_dupl[f[1]]++
+        }
       else
-        tt[f[1]]=f[2];
+        { tt[f[1]]=NR;
+          n_dupl[f[1]]=1;
+        }
     }
   if(body)
     { # for(t in tt)
@@ -41,19 +47,43 @@ gawk -v LAYOUT_FILE=$1 'BEGIN { layout_file=LAYOUT_FILE;
            # print i, length(f[i]), f[i];
            if(index(f[i],"\"")!=0 && length(f[i])>0)
              if(!(f[i] in tt))
-               { err++;
-                 printf "Potentially missing tooltip - line #%i : %s", NR, f[i]; # > "/dev/stderr";
+               { err++; m_err++;
+                 lines_missing[f[i]]=lines_missing[f[i]]" "NR;
+                 n_missing[f[i]]++;
+                 # printf "Potentially missing tooltip - line #%i : %s", NR, f[i]; # > "/dev/stderr";
                  n_quotes=gsub("\"","\"",f[i]);
                  if(match(f[i],"^\"[^\"]+\"$")==0 || index(n_quotes/2,".5")!=0)
-                   printf " + Unmatched double quotes (n=%i)", n_quotes*1; # > "/dev/stderr";
-                 printf "\n";
+                   { quotes[f[i]]=quotes[f[i]]" "NR":"n_quotes*1;
+                     q_err++;
+                     # printf " + Unmatched double quotes (n=%i)", n_quotes*1; # > "/dev/stderr";
+                   }
+                 # printf "\n";
                }
          }
     }
   if(match($0,"^[-][-]")!=0)
     { body=1; tooltips=0; }
 }
-END { if(err==0)
+END { if(d_err!=0)
+      { print "Duplicate tooltip specification(s):";
+        PROCINFO["sorted_in"]="@val_num_desc"
+        for(t in n_dupl)
+           if(n_dupl[t]>1)
+             print n_dupl[t]" lines - "t" : "tt[t];
+      }
+
+      if(m_err>0)
+        { print "Potentially missing tooltip specification(s):";
+          PROCINFO["sorted_in"]="@val_num_desc"
+          for(t in n_missing)
+             print n_missing[t]" lines - "t" : "lines_missing[t];
+        }
+      if(q_err>0)
+      { print "Unmatched double quotes";
+        for(t in quotes)
+           print t" - lines: "quotes[t];
+      }
+      if(err==0)
         { print "SUMMARY: No inconsistencies observed in ",NR," lines."; # > "/dev/stderr";
           exit 0; }
       else
@@ -61,4 +91,3 @@ END { if(err==0)
           exit 1;
         }
     }'
-
