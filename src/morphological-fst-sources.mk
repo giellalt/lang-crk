1fe6230633b3bb3d800e1f67374cb841663344c0
Sjur N Moshagen <sjurnm@mac.com>
2019-05-02 12:04:50 +0000

    Updated E Santos’ make file to always use the same source files as the main build system.

--- src/morphological-fst-sources.mk
+++ src/morphological-fst-sources.mk
@@ -17,15 +17,6 @@
 # Lists the raw .lexc/.twolc/.regex sources required to create the
 # morphological FSTs.
 
-MORPHOLOGY = morphology/root.lexc \
-    morphology/affixes/noun_affixes.lexc \
-    morphology/affixes/verb_affixes.lexc \
-    morphology/stems/abbreviations.lexc \
-    morphology/stems/crk-propernouns.lexc \
-    morphology/stems/noun_stems.lexc \
-    morphology/stems/numerals.lexc \
-    morphology/stems/particles.lexc \
-    morphology/stems/pronouns.lexc \
-    morphology/stems/verb_stems.lexc
+MORPHOLOGY = morphology/lexicon.tmp.lexc
 PHONOLOGY = phonology/crk-phon.twolc
 ORTHOGRAPHY = orthography/spellrelax.regex
