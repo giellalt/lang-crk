96c1a19eddc7b7c9478ec787836d83210a519a61
Antti Arppe <arppe@ualberta.ca>
2020-01-08 09:10:58 +0000

    Added filter to rule out excessive sequences of V(G) morphemes (that collapse); added removal of flag diacritic concerning morphemes when used to rule out proximate morpheme repetitions; added some progress reporting; specified variable -w to allow for the creation of different variants of derivational+inflectional FST.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/crk@186629 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- src/derivation/crk-drv+infl.sh
+++ src/derivation/crk-drv+infl.sh
@@ -1,36 +1,54 @@
 #!/bin/sh
 
-hfst-lexc crk-drv.lexc -o crk-drv-morph.hfst
-echo 'Compiled derivational morphology.' ;
+hfst-lexc $GTLANG_crk/src/derivation/crk-drv-weighted.lexc -o crk-drv-morph-w.hfst
+echo 'CRK-DRV: Compiled derivational morphology.' ;
 
-hfst-xfst -F crk-drv.regex
+hfst-xfst -F $GTLANG_crk/src/derivation/crk-drv.regex
 echo 'Compiled derivational phonology.' ;
 
-hfst-xfst -F crk-morphophon.regex
-echo 'Compiled derivational morphophonology' ;
+hfst-xfst -F $GTLANG_crk/src/derivation/crk-morphophon.regex
+echo 'CRK-DRV: Compiled derivational morphophonology' ;
 
-hfst-compose -F -x ON -1 crk-drv-morph.hfst -2 crk-drv-phon.hfst -o - |
-hfst-compose -x ON -1 - -2 crk-morphophon.hfst -o crk-drv.hfst
-echo 'Composed together derivational morphology and phonology.' ;
+echo '~[ $[ [ a | â | ê | i | î | o | ô ] ( [ w | y | ý ] ) [ "/" [ a | â | ê | i | î | o | ô ] ( [ w | y | ý ] ) ]^2 ] ] ;' |
+hfst-regexp2fst -S -o crk-drv-VG3-filter.hfst
+echo 'CRK-DRV: Compiled filter to exclude excessive VG cases.';
+
+hfst-compose -F -x ON -1 crk-drv-morph-w.hfst -2 crk-drv-VG3-filter.hfst -o - |
+hfst-compose -F -x ON -1 - -2 crk-drv-phon.hfst -o - |
+hfst-compose -x ON -1 - -2 crk-morphophon.hfst -o crk-drv-w.hfst
+
+# hfst-compose -F -x ON -1 crk-drv-morph-w.hfst -2 crk-drv-phon.hfst -o - |
+# hfst-compose -x ON -1 - -2 crk-morphophon.hfst -o crk-drv-w.hfst
+echo 'CRK-DRV: Composed together derivational morphology and phonology.' ;
 
 cat $GTLANG_crk/src/morphology/root.lexc $GTLANG_crk/src/morphology/affixes///noun_affixes.lexc $GTLANG_crk/src/morphology/affixes///propernouns.lexc $GTLANG_crk/src/morphology/affixes///verb_affixes.lexc $GTLANG_crk/src/morphology/stems///abbreviations.lexc $GTLANG_crk/src/morphology/stems///crk-propernouns.lexc $GTLANG_crk/src/morphology/stems///noun_stems.lexc $GTLANG_crk/src/morphology/stems///numerals.lexc $GTLANG_crk/src/morphology/stems///particles.lexc $GTLANG_crk/src/morphology/stems///pronouns.lexc $GTLANG_crk/src/morphology/stems///verb_stems.lexc $GTLANG_crk/src/morphology///stems/derivation_stems.lexc > crk-infl.lexc
 
 hfst-lexc crk-infl.lexc -o crk-infl-morph.hfst
-echo 'Compiled inflectional morphology.' ;
+echo 'CRK-DRV: Compiled inflectional morphology.' ;
 
-hfst-substitute -i crk-infl-morph.hfst -o crk-drv-infl-morph.hfst -f 'DRV-FST' -T crk-drv.hfst
-echo 'Substituted DRV-FST with derivational model.' ;
+hfst-substitute -i crk-infl-morph.hfst -o crk-drv-infl-morph-w.hfst -f 'DRV-FST' -T crk-drv-w.hfst
+echo 'CRK-DRV: Substituted DRV-FST with derivational model.' ;
 
-hfst-xfst -e "load crk-drv-infl-morph.hfst" -e "eliminate flag infl" -E "save stack crk-drv-infl-morph.hfst"
+hfst-xfst -e "load crk-drv-infl-morph-w.hfst" -e "eliminate flag infl" -e "eliminate flag morf" -E "save stack crk-drv-infl-morph-w.hfst"
 
 # hfst-eliminate-flags -F infl -i crk-drv-infl-morph.hfst -o crk-drv-infl-morph2.hfst 
-echo 'Removed flag: infl' ;
+echo 'CRK-DRV: Removed flag: infl' ;
+echo 'CRK-DRV: Removed flag: morf' ;
 
-hfst-twolc -i $GTLANG_crk/src/phonology/crk-phon.twolc -o crk-phon.hfst
-echo 'Compiled inflectional morphophonological TWOLC rules.'
+# hfst-twolc -i $GTLANG_crk/src/phonology/crk-phon.twolc -o crk-phon.hfst
+# echo 'Compiled inflectional morphophonological TWOLC rules.'
 
-echo 'Composing and intersecting together inflectional morphology and phonology.'
-hfst-compose-intersect crk-drv-infl-morph.hfst crk-phon.hfst |
-hfst-minimize -i - -o crk-gen-norm.hfst
+echo 'CRD-DRV: Composing and intersecting together inflectional morphology and phonology.'
+hfst-compose-intersect crk-drv-infl-morph-w.hfst crk-phon.hfst |
+hfst-minimize -i - -o crk-gen-norm-w.hfst
 
-echo 'Done' ;
+echo 'CRT-DRV: Inverting generating FST to create analyzing FST.' ;
+
+hfst-invert -i crk-gen-norm-w.hfst -o crk-anl-norm-w.hfst
+
+echo 'CRK-DRV: Creating optimized versions of both FSTs.' ;
+
+hfst-fst2fst -w -i crk-gen-norm-w.hfst -o crk-gen-norm-w.hfstol
+hfst-fst2fst -w -i crk-anl-norm-w.hfst -o crk-anl-norm-w.hfstol
+
+echo 'CRK-DRV: Done' ;
