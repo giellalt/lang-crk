02c8f7ef34f5fbc79bf560140f2759e9f6d33425
Antti Arppe <arppe@ualberta.ca>
2020-04-05 18:57:47 +0000

    Revised script with  variable, as well as slight modifications to deal with new AEW coding, requiring 'twosided flag-diacritics' after composing derivational model with the inflectional one.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/crk@189265 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- src/derivation/crk-drv+infl.sh
+++ src/derivation/crk-drv+infl.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-hfst-lexc $GTLANG_crk/src/derivation/crk-drv-weighted.lexc -o crk-drv-morph-w.hfst
+SUFFIX="";
+
+hfst-lexc $GTLANG_crk/src/derivation/crk-drv.lexc -o crk-drv-morph$SUFFIX.hfst
 echo 'CRK-DRV: Compiled derivational morphology.' ;
 
 hfst-xfst -F $GTLANG_crk/src/derivation/crk-drv.regex
@@ -13,12 +15,12 @@ echo '~[ $[ [ a | â | ê | i | î | o | ô ] ( [ w | y | ý ] ) [ "/" [ a | â 
 hfst-regexp2fst -S -o crk-drv-VG3-filter.hfst
 echo 'CRK-DRV: Compiled filter to exclude excessive VG cases.';
 
-hfst-compose -F -x ON -1 crk-drv-morph-w.hfst -2 crk-drv-VG3-filter.hfst -o - |
+hfst-compose -F -x ON -1 crk-drv-morph$SUFFIX.hfst -2 crk-drv-VG3-filter.hfst -o - |
 hfst-compose -F -x ON -1 - -2 crk-drv-phon.hfst -o - |
-hfst-compose -x ON -1 - -2 crk-morphophon.hfst -o crk-drv-w.hfst
+hfst-compose -x ON -1 - -2 crk-morphophon.hfst -o crk-drv$SUFFIX.hfst
 
-# hfst-compose -F -x ON -1 crk-drv-morph-w.hfst -2 crk-drv-phon.hfst -o - |
-# hfst-compose -x ON -1 - -2 crk-morphophon.hfst -o crk-drv-w.hfst
+# hfst-compose -F -x ON -1 crk-drv-morph$SUFFIX.hfst -2 crk-drv-phon.hfst -o - |
+# hfst-compose -x ON -1 - -2 crk-morphophon.hfst -o crk-drv$SUFFIX.hfst
 echo 'CRK-DRV: Composed together derivational morphology and phonology.' ;
 
 cat $GTLANG_crk/src/morphology/root.lexc $GTLANG_crk/src/morphology/affixes///noun_affixes.lexc $GTLANG_crk/src/morphology/affixes///propernouns.lexc $GTLANG_crk/src/morphology/affixes///verb_affixes.lexc $GTLANG_crk/src/morphology/stems///abbreviations.lexc $GTLANG_crk/src/morphology/stems///crk-propernouns.lexc $GTLANG_crk/src/morphology/stems///noun_stems.lexc $GTLANG_crk/src/morphology/stems///numerals.lexc $GTLANG_crk/src/morphology/stems///particles.lexc $GTLANG_crk/src/morphology/stems///pronouns.lexc $GTLANG_crk/src/morphology/stems///verb_stems.lexc $GTLANG_crk/src/morphology///stems/derivation_stems.lexc > crk-infl.lexc
@@ -26,10 +28,10 @@ cat $GTLANG_crk/src/morphology/root.lexc $GTLANG_crk/src/morphology/affixes///no
 hfst-lexc crk-infl.lexc -o crk-infl-morph.hfst
 echo 'CRK-DRV: Compiled inflectional morphology.' ;
 
-hfst-substitute -i crk-infl-morph.hfst -o crk-drv-infl-morph-w.hfst -f 'DRV-FST' -T crk-drv-w.hfst
+hfst-substitute -i crk-infl-morph.hfst -o crk-drv-infl-morph$SUFFIX.hfst -f 'DRV-FST' -T crk-drv$SUFFIX.hfst
 echo 'CRK-DRV: Substituted DRV-FST with derivational model.' ;
 
-hfst-xfst -e "load crk-drv-infl-morph-w.hfst" -e "eliminate flag infl" -e "eliminate flag morf" -E "save stack crk-drv-infl-morph-w.hfst"
+hfst-xfst -e "load crk-drv-infl-morph$SUFFIX.hfst" -e "twosided flag-diacritics" -e "eliminate flag infl" -e "eliminate flag morf" -E "save stack crk-drv-infl-morph$SUFFIX.hfst"
 
 # hfst-eliminate-flags -F infl -i crk-drv-infl-morph.hfst -o crk-drv-infl-morph2.hfst 
 echo 'CRK-DRV: Removed flag: infl' ;
@@ -39,16 +41,16 @@ echo 'CRK-DRV: Removed flag: morf' ;
 # echo 'Compiled inflectional morphophonological TWOLC rules.'
 
 echo 'CRD-DRV: Composing and intersecting together inflectional morphology and phonology.'
-hfst-compose-intersect crk-drv-infl-morph-w.hfst crk-phon.hfst |
-hfst-minimize -i - -o crk-gen-norm-w.hfst
+hfst-compose-intersect crk-drv-infl-morph$SUFFIX.hfst crk-phon.hfst |
+hfst-minimize -i - -o crk-gen-norm$SUFFIX.hfst
 
 echo 'CRT-DRV: Inverting generating FST to create analyzing FST.' ;
 
-hfst-invert -i crk-gen-norm-w.hfst -o crk-anl-norm-w.hfst
+hfst-invert -i crk-gen-norm$SUFFIX.hfst -o crk-anl-norm$SUFFIX.hfst
 
 echo 'CRK-DRV: Creating optimized versions of both FSTs.' ;
 
-hfst-fst2fst -w -i crk-gen-norm-w.hfst -o crk-gen-norm-w.hfstol
-hfst-fst2fst -w -i crk-anl-norm-w.hfst -o crk-anl-norm-w.hfstol
+hfst-fst2fst -w -i crk-gen-norm$SUFFIX.hfst -o crk-gen-norm$SUFFIX.hfstol
+hfst-fst2fst -w -i crk-anl-norm$SUFFIX.hfst -o crk-anl-norm$SUFFIX.hfstol
 
 echo 'CRK-DRV: Done' ;
