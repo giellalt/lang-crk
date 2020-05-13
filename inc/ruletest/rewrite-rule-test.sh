d77cb54babb9014ad5f4ed06bfe1b6fe85143557
antti.arppe@iki.fi <antti.arppe@iki.fi>
2020-05-13 04:57:10 +0000

    Split output into a long vs. non-long form, where the long format provides rule-by-rule analysis of the results of the individual rewrite rules, whereas the non-long form only provides the case-wise scrutiny of the success/failure of the aggregate of the rules (provided in long form). In either case, an overall summary is presented at the end.

--- inc/ruletest/rewrite-rule-test.sh
+++ inc/ruletest/rewrite-rule-test.sh
@@ -7,7 +7,8 @@
 # as well as an optional second space-separated form representing the
 # final outcome.
 
-gawk -v XFSCRIPT=$1 'BEGIN { xfscript=XFSCRIPT;
+gawk -v XFSCRIPT=$1 -v REPORT=$2 'BEGIN { xfscript=XFSCRIPT;
+report=REPORT;
   while((getline < xfscript)!=0)
   { if(index($0,"regex")!=0)
       { rx=1; regex=""; }
@@ -27,35 +28,49 @@ gawk -v XFSCRIPT=$1 'BEGIN { xfscript=XFSCRIPT;
       gsub("0","",target);
     }
   gsub("\\.o\\.","->",regex);
-  print "REWRITE RULE SEQUENCE:";
-  print regex;
-  print "";
-  print "0 - "input;
-  print "---";
-  for(i=1; i<=n; i++)
-     { flookup="flookup -q -b -i "rule[i]".foma";
-       print input |& flookup;
-       flookup |& getline;
-       close(flookup);
-       if($1==$2)
-         diff="-";
-       else
-         diff="|";
-       print i" "diff" "$2" - "rule[i];
+  if(report=="long")
+    { print "REWRITE RULE SEQUENCE:";
+      print regex;
+      print "";
+      print "0 - "input;
+      print "---";
+    }
+    for(i=1; i<=n; i++)
+       { flookup="flookup -q -b -i "rule[i]".foma";
+         print input |& flookup;
+         fflush(); close(flookup, "to");
+         flookup |& getline;
+         fflush(); close(flookup, "from");
+         if($1==$2)
+           diff="-";
+         else
+           diff="|";
+         if(report=="long")
+           print i" "diff" "$2" - "rule[i];
        # print i" "diff" "rule[i]": "$1" -> "$2;
-       input=$2;
-     }
-  print "---";
+         input=$2;
+       }
+     if(report=="long")
+       print "---";
   output=$2;
   gsub("[<>]","",output);
   if(target!="")
     if(output==target)
-      success="(=)";
+      { success="(=)";
+        n_success++;
+      }
     else
-      success="(<> "target")";
+      { success="(<> "target")";
+        n_fail++;
+      }
   else
-      success="";
-  print "1-"n": "lexc" -> "$2" "success;
-  print "";
+      { success="";
+        n_other++;
+      }
+  print NR": 1-"n": "lexc" -> "$2" "success;
+  if(report=="long")
+    print "";
+}
+END { printf "SUMMARY - SUCCESS: %i/%i - FAIL: %i/%i - OTHER: %i/%i\n", n_success, NR, n_fail, NR, n_other, NR;
 }'
 
