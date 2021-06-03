779fe8b80bcd5361be505824c254f1c26ed6d269
antti.arppe@iki.fi <antti.arppe@iki.fi>
2021-06-03 02:33:57 -0600

    Revised and improved code, to add new features (<gwc> for general wordclasses), fix the inclusion of lemmas and paradigm (layout) references only for inflecting wordclasses. Some manual validation of the code still necessary.

--- tools/shellscripts/add-explicit-fields-to-crkeng.sh
+++ tools/shellscripts/add-explicit-fields-to-crkeng.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-gawk -v FSTNORMANL=$1 '{ fstnormanl=FSTNORMANL;
+gawk -v FSTNORMANL=$1 '{ fstnormanl=FSTNORMANL; NULL="";
 FS="\n"; RS=""; }
 NR<4 { print; print ""; }
 NR>=4 {
   match($0, "pos=\"[^\"]+\">([^<]+)</l>.*<lc>([^<]+)</lc>", f);
   head=f[1];
   lc=f[2];
+
+  head0=head;
+  sub("[[:punct:]]+$","",head0);
 
   swc=lc;
   sub("-.+","",swc);
@@ -15,74 +18,80 @@ NR>=4 {
   gwc=c[1];
   
   cmd="hfst-optimized-lookup -q "fstnormanl;
-  print head |& cmd;
+  print head0 |& cmd;
   fflush(); close(cmd,"to");
-  cmd |& getline anls;
+  cmd |& getline fstanls;
   fflush(); close(cmd,"from");
 
-  lemma="NULL"; disamb="NULL"; maxmorf=100;
-  nf=split(anls,anl,"\n");
+  lemma=NULL; disamb=NULL; maxmorf=100;
+  nf=split(fstanls,fstanl,"\n");
   for(i=1; i<=nf; i++)
-     if(index(anl[i],"+Err")==0 && index(anl[i],"+?")==0)
-       { 
-         match(anl[i],"^(((IC)|(Rdpl[WS])|(PV/[^\\+]+))\\+)*([^\\+]+)\\+([^\\+])",f);
+     if(index(fstanl[i],"+Err")==0 && index(fstanl[i],"+?")==0)
+       {
+         split(fstanl[i], anl, "\t");
+         match(anl[2],"^(((IC)|(Rdpl[WS])|(PV/[^\\+]+))\\+)*([^\\+]+)\\+([^\\+])",f);
          anlwc=f[7];
          if(anlwc=="N")
            {
-             if(index(anl[i],"+D+")!=0)
+             if(index(anl[2],"+D+")!=0)
                anlswc=anlwc "D";
-             if(index(anl[i],"+A+")!=0)
+             if(index(anl[2],"+A+")!=0)
                anlswc=anlwc "A";
-             if(index(anl[i],"+I+")!=0)
+             if(index(anl[2],"+I+")!=0)
                anlswc=anlwc "I";
            }
          if(anlwc=="V")
            {
-             if(index(anl[i],"II")!=0)
+             if(index(anl[2],"II")!=0)
                anlswc="VII";
-             if(index(anl[i],"AI")!=0)
+             if(index(anl[2],"AI")!=0)
                anlswc="VAI";
-             if(index(anl[i],"TI")!=0)
+             if(index(anl[2],"TI")!=0)
                anlswc="VTI";
-             if(index(anl[i],"TA")!=0)
+             if(index(anl[2],"TA")!=0)
                anlswc="VTA";
            }
-         nm=gsub("\\+","+",anl[i]); # print "SWC: "anlswc;
+         nm=gsub("\\+","+",anl[2]); # print "SWC: "anlswc;
          if(((anlwc=="I" && anlwc==gwc) || ((anlwc=="V" || anlwc=="N") && anlswc==swc) || (anlwc=="P" && gwc=="P")) && nm<maxmorf)
            {
-             split(f[6],ff,"\t");
-             lemma=ff[2];
+             # split(f[6],ff,"\t");
+             # lemma=ff[2];
+             lemma=f[6];
              maxmorf=nm;
-             split(anl[i],ff,"\t");
-             disamb=ff[2];
+             # split(anl[i],ff,"\t");
+             # disamb=ff[2];
+             disamb=anl[2];
            }
        }
 
-  if(lemma==head)
+  if(lemma==head0)
     is_lemma="TRUE";
   else
     is_lemma="FALSE";
 
   type="NULL";
-  if(lemma==head && index(head," ")==0)
+  if(lemma==head0 && index(head0," ")==0)
     type="lemma";
-  if(lemma!=head && index(head," ")==0 && (swc!="IPV" && swc!="IPN" || swc!="IPP"))
+  if(lemma!=head0 && index(head0," ")==0 && (swc!="IPV" && swc!="IPN" || swc!="IPP"))
     type="word";
-  if(lemma!=head && index(head," ")==0 && (swc=="IPV" || swc=="IPN" || swc=="IPP"))
+  if(lemma!=head0 && index(head0," ")==0 && (swc=="IPV" || swc=="IPN" || swc=="IPP"))
     type="morpheme";
-  if(index(head," ")!=0)
+  if(index(head0," ")!=0)
     type="phrase";
 
-  paradigms="NULL";
+  if(type=="phrase")
+    lemma=NULL;
+  paradigms=NULL;
   if(type=="lemma" && (gwc=="N" || gwc=="V"))
-    paradigms=tolower(swc)"-basic.layout, "tolower(swc)"-full.layout";
+    paradigms="\n        <paradigm>"tolower(swc)"-basic</paradigm>\n        <paradigm>"tolower(swc)"-full</paradigm>\n      ";
   if(index(disamb,"Pron+Pers")!=0)
-    paradigms="pronoun-personal.layout";
+    paradigms="personal-pronouns";
   if(index(disamb,"Pron+Dem")!=0)
-    paradigms="pronoun-demonstrative.layout";
+    paradigms="demonstrative-pronouns";
 
-  newfields="      <type>"type"</type>\n      <swc>"swc"</swc>\n      <anl>"disamb"</anl>\n      <lemma>"lemma"</lemma>\n      <paradigms>"paradigms"</paradigms>";
+  newfields="      <type>"type"</type>\n      <gwc>"gwc"</gwc>\n      <swc>"swc"</swc>\n      <anl>"disamb"</anl>\n      <lemma>"lemma"</lemma>\n      <paradigms>"paradigms"</paradigms>";
   sub("</lc>", "</lc>\n"newfields,$0);
-  print $0; print ""; # print lemma, disamb, is_lemma, swc, gwc, paradigms;
+  print $0; print "";
+  # print lemma, disamb, is_lemma, swc, gwc, paradigms;
 }'
 
