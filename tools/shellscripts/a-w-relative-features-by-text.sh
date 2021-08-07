aa8bc8b3d2167303d9945e91b2cf7837c2a87629
antti.arppe@iki.fi <antti.arppe@iki.fi>
2021-08-07 14:46:49 +0300

    Fixed counting of word frequency for each text.

--- tools/shellscripts/a-w-relative-features-by-text.sh
+++ tools/shellscripts/a-w-relative-features-by-text.sh
@@ -5,13 +5,12 @@ if(match($0,"^<text id=\"([^\"]+)",f)!=0)
   { text_id=f[1];
   }
 if(match($0,"^<")==0)
-  { freq[text_id]++;
+  { n_texts[text_id]++;
     nf=split($0,f,"\t");
     nff=split(f[2],ff);
     # Going through non-lemma features
     for(i=2; i<=nff; i++)
        { n_features[ff[i]]++;
-         n_texts[text_id]++;
          n_features_by_text[text_id, ff[i]]++;
        }
   }
