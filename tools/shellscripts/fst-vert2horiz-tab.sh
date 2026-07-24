323f9d705d4e8fc2457e2a0a84c4f1a27fdc1aa4
aarppe <antti.arppe@iki.fi>
2026-07-24 14:57:18 +0300

    Streamlining of code.

--- tools/shellscripts/fst-vert2horiz-tab.sh
+++ tools/shellscripts/fst-vert2horiz-tab.sh
@@ -3,10 +3,18 @@
 # cat ~/altlab/crk/corpora/ahenakew_wolfart.vrt | egrep -v '^<' | hfst-optimized-lookup -q src/analyser-gt-norm.hfstol |
 
 gawk 'BEGIN { FS="\n"; RS=""; }
-{ split($1,f,"\t");
+$0 ~ /^</ { print; }
+$0 ~ /^[^<]/ {
+  split($1,f,"\t");
   printf "%s", f[1];
   if(f[3]=="+?")
-    printf "\t%s%s", f[2], f[3];
+    if(match(f[2],"^[[:punct:]]$")!=0)
+      if(match(f[2],"\\.|,|!|\\?|;|:")!=0)
+        printf "\t%s+CLB", f[2];
+      else        
+        printf "\t%s+PUNCT", f[2];
+    else
+      printf "\t%s%s", f[2], f[3];
   else
     for(i=1; i<=NF; i++)
        { split($i,f,"\t");
