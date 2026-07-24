200e6d5d27b4d3392ac96cf25c764e88443fb337
aarppe <antti.arppe@iki.fi>
2026-07-24 14:56:07 +0300

    Reimplemented reintegration of lemma so that this happens only once for an analysis string (needed for derived forms).

--- tools/shellscripts/cg-anl2fst-anl.sh
+++ tools/shellscripts/cg-anl2fst-anl.sh
@@ -4,19 +4,31 @@
 
 # Turns a CG formatted analysis back into standard XFST analysis
 
-gawk -F"\t" '{ anl=$2;
+gawk -F"\t" '$0 ~ /^[<]/
+$0 ~ /^[^<]/ { anl=$2;
   sub("@.*$","",anl);
   sub("Quot.*$","",anl);
   sub("((TI)|(IIZ)) $","",anl);
   sub("Ipc.*$","Ipc",anl);
   sub("\\<Num\\>","",anl);
+
   n=split(anl,a," ");
+  lemma=a[1];
   new_anl="";
+  lemma_ins=0;
+
   for(i=2; i<=n; i++)
-     if(a[i]=="V" || a[i]=="N" || a[i]=="Ipc" || a[i]=="Pron" || a[i]=="CLB" || a[i]=="PUNCT" || a[n]=="?")
-       new_anl=new_anl"+"a[1]"+"a[i];
+     if((a[i]=="V" || a[i]=="N" || a[i]=="Ipc" || a[i]=="Iph" || a[i]=="Ipn" || a[i]=="Pron" || a[i]=="CLB" || a[i]=="PUNCT" || a[n]=="?") && !lemma_ins)
+       {
+         new_anl=new_anl"+"a[1]"+"a[i];
+         lemma_ins=1;
+       }
      else
        new_anl=new_anl"+"a[i];
   sub("^\\+","",new_anl);
+
+  # Adding surface-syntactic CG tags (first one that is matched):
+  if(match($2,"@[^ ]+",g)!=0)
+    new_anl=new_anl "\t" g[0];
   print $1"\t"new_anl;
 }'
